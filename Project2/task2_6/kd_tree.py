d7c53cce3e43f03e316e2fc311643f142ed3146a
saikat-roy <saikatroy@uni-bonn>
2019-05-30 01:44:57 +0200

    Bayesian Regression completed on MAP. MLE not working at the moment (but not needed either)

--- Project2/task2_6/kd_tree.py
+++ Project2/task2_6/kd_tree.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import linalg as la
 
 class KDTree:
 
@@ -8,8 +9,9 @@ class KDTree:
         self.X = None
         self.Y = None
         self.root = None
+        self.depth = None
 
-    def train(self, x, y, dim_mode='alternate', split_mode='mid'):
+    def train(self, x, y, dim_mode='alternate', split_mode='mid', depth=3):
         """
         :param x: ndarray
         :param y: ndarray
@@ -19,26 +21,28 @@ class KDTree:
         """
         self.X = x
         self.Y = y
+        self.depth = depth
         idxs = np.array([i for i in range(self.X.shape[0])])
-        self.root = self._train(idxs=idxs, dim_mode=dim_mode, split_mode=split_mode)
+        self.root = self._train(idxs=idxs, depth=1, dim_mode=dim_mode, split_mode=split_mode)
 
-    def _train(self, idxs, dim_mode='alternate', split_mode='mid'):
+    def _train(self, idxs, depth, dim_mode='alternate', split_mode='mid'):
 
         dim = self.select_dim(dim_mode=dim_mode, idxs=idxs if dim_mode == 'var' else None)
         split_point = self.select_split(split_mode=split_mode, split_dim=dim, idxs=idxs)
 
-        if idxs.shape[0]==1:
-            return KDNode(k=None, val=None, idx=idxs)
+        if depth>=self.depth:
+            return KDNode(split_dim=None, val=None, idx=idxs)
 
         # Only if there is a split to be made
-        node = KDNode(k=dim, val=split_point)
+        node = KDNode(split_dim=dim, val=split_point)
 
-        left_idxs = idxs[self.X[idxs]<=split_point]
-        node.left_node = self._train(idxs=left_idxs, dim_mode=dim_mode, split_mode=split_mode) if left_idxs.shape[0]>0 \
+        #print(self.X[idxs, dim] <= split_point)
+        left_idxs = idxs[self.X[idxs,dim]<=split_point]
+        node.left_node = self._train(idxs=left_idxs, depth=depth+1, dim_mode=dim_mode, split_mode=split_mode) if left_idxs.shape[0]>0 \
                                 else None
 
-        right_idxs = idxs[self.X[idxs] > split_point]
-        node.right_node = self._train(idxs=right_idxs, dim_mode=dim_mode, split_mode=split_mode) if \
+        right_idxs = idxs[self.X[idxs, dim] > split_point]
+        node.right_node = self._train(idxs=right_idxs, depth=depth+1, dim_mode=dim_mode, split_mode=split_mode) if \
                             right_idxs.shape[0] > 0 else None
 
         return node
@@ -56,6 +60,7 @@ class KDTree:
             raise ValueError # Choose 'alternate' or 'var' for dimension choice
 
     def select_split(self, split_mode, split_dim, idxs):
+        # To implement: Point closest to split point and not point itself.
         if split_mode == 'mid':
             return np.mean(self.X[idxs, split_dim], axis=0)
         elif split_mode == 'median':
@@ -63,21 +68,36 @@ class KDTree:
         else:
             raise ValueError # Choose split point as 'mid' or 'median' point of data
 
-    def __str__(self):
-        raise NotImplementedError
-        print(self.root.val, self.root.k)
+    # def __str__(self):
+    #     raise NotImplementedError
+    #     print(self.root.val, self.root.k)
+
+    def search(self):
+        return
 
     def plot(self):
         assert self.k == 2
         raise NotImplementedError
-    
+
 
 class KDNode:
 
-    def __init__(self, k, val, idx):
+    def __init__(self, split_dim, val, idxs=None):
 
-        self.k = k
+        self.split_dim = split_dim
         self.val = val
-        self.idx = idx
+        self.idxs = idxs # Only populated for leaf nodes
         self.left_node = None
         self.right_node = None
+
+
+if __name__ == "__main__":
+
+    with open("data2-train.dat", "r") as f:
+        d = np.loadtxt(f)
+
+    X = d[:,0:2]
+    Y = d[:,2]
+    model = KDTree(k=2)
+    model.train(x=X, y=Y, depth=3)
+
