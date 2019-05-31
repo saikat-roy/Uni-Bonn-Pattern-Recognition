5c3c362a05321190a6e076bf3024c98d9fa2ddb3
Sarah0711 <sarahjmi07@gmail.com>
2019-05-31 23:53:12 +0200

    not working for order 5

--- Project2/task2_1/task_2_1.py
+++ Project2/task2_1/task_2_1.py
@@ -3,83 +3,25 @@ import scipy as sp
 import matplotlib.pyplot as plt
 
 
-def plotData2D(X, filename=None):
-    # create a figure and its axes
-    fig = plt.figure()
-    axs = fig.add_subplot(111)
-
-    # see what happens, if you uncomment the next line
-    # axs.set_aspect('equal')
-
-    # plot the data
-    axs.plot(X[0, :], X[1, :], 'ro', label='data')
-
-    # set x and y limits of the plotting area
-    xmin = X[0, :].min()
-    xmax = X[0, :].max()
-    axs.set_xlim(xmin - 10, xmax + 10)
-    axs.set_ylim(-2, X[1, :].max() + 10)
-
-    # set properties of the legend of the plot
-    leg = axs.legend(loc='upper left', shadow=True, fancybox=True, numpoints=1)
-    leg.get_frame().set_alpha(0.5)
-
-    # either show figure on screen or write it to disk
-    if filename == None:
-        plt.show()
-    else:
-        plt.savefig(filename, facecolor='w', edgecolor='w',
-                    papertype=None, format='pdf', transparent=False,
-                    bbox_inches='tight', pad_inches=0.1)
-    plt.close()
-
 
 if __name__ == "__main__":
     #######################################################################
     # 1st alternative for reading multi-typed data from a text file
     #######################################################################
     # define type of data to be read and read data from file
-    dt = np.dtype([('w', np.float), ('h', np.float), ('g', np.str_, 1)])
-    data = np.loadtxt('whData.dat', dtype=dt, comments='#', delimiter=None)
 
-    # read height, weight and gender information into 1D arrays
-    ws = np.array([d[0] for d in data])
-    hs = np.array([d[1] for d in data])
-    gs = np.array([d[2] for d in data])
-
-    ##########################################################################
-    # 2nd alternative for reading multi-typed data from a text file
-    ##########################################################################
-    # read data as 2D array of data type 'object'
     data = np.loadtxt('whData.dat', dtype=np.object, comments='#', delimiter=None)
+    # Removing rows with missing weights
+    data = data[data[:, 0] != '-1', :]
 
-    # read height and weight data into 2D array (i.e. into a matrix)
-    X = data[:, 0:2].astype(np.float)
-    # read gender data into 1D array (i.e. into a vector)
-    y = data[:, 2]
+    # read height data into 1D array (i.e. into a matrix)
+    X = data[:, 1].astype(np.float)
 
-    # let's transpose the data matrix
-    X = X.T
+    # read weight data into 1D array (i.e. into a vector)
+    y = data[:, 0].astype(np.float)
 
-    # now, plot weight vs. height using the function defined above
-    plotData2D(X, 'plotWH.pdf')
-
-    # next, let's plot height vs. weight
-    # first, copy information rows of X into 1D arrays
-    w = np.copy(X[0, :])
-    h = np.copy(X[1, :])
-
-    index = np.where(w<0)
-    #print(index)
-    h = np.delete(h, index)
-    #print(len(h))
-
-    w = w[w>=0]
-    #print(len(w))
-    row=[]
-
-
-
+    plt.scatter(X, y, color='black', label='Data')
+    w=x
     def get_matrices(w,h,n):
         X1 = np.zeros((len(h),n+1))
         #print(X)
@@ -97,10 +39,10 @@ if __name__ == "__main__":
     ztrans_X1[0,:] = 1.0
     inter1 = ztrans_X1.transpose() * ztrans_X1
     #print(inter1)
-    inter2 = np.linalg.pinv(inter1)
+    inter2 = np.linalg.inv(inter1)
     #id = np.identity(11)
     #inter2 = np.linalg.solve(inter1,id)
-    inter3 = np.mat(inter2) * np.mat(X1.transpose())
+    inter3 = np.mat(inter2) * np.mat(ztrans_X1.transpose())
     W = np.mat(inter3) * np.mat(w).transpose()
     #print(W)
 
