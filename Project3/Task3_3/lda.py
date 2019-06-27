89aec0dd47c6309167ac13a1d9e75bc658af4d40
saikat-roy <saikatroy@uni-bonn>
2019-06-28 01:47:15 +0200

    LDA (and PCA) 2D and 3D done. Weird result from 3D needs analysis.

--- Project3/Task3_3/lda.py
+++ Project3/Task3_3/lda.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.linalg import eig
+from numpy.linalg import eig
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D # <--- This is important for 3d plotting
 # from mpl_toolkits import mplot3d
@@ -9,36 +9,49 @@ if __name__ == "__main__":
     X = np.loadtxt('data-dimred-X.csv', dtype=np.float, delimiter=',')
     y = np.loadtxt('data-dimred-y.csv', dtype=np.float)
 
-    # X = X.T
-    X = X-np.expand_dims(X.mean(axis=1),1)
+    #X = X.T
+    # X = X-np.expand_dims(X.mean(axis=1),1)
 
-    C = np.dot(X,X.T)/y.shape[0]
-    l, U = eig(C)
+    S_w = np.zeros((X.shape[0], X.shape[0]), dtype=np.float)
+    S_b = np.zeros((X.shape[0], X.shape[0]), dtype=np.float)
+    mu = np.expand_dims(np.mean(X,axis=1), 1)
 
-    # 2D PCA Projections
+    for cl in np.unique(y):
+        X_cl = X[:,y==cl]
+        mu_cl  = np.expand_dims(np.mean(X_cl, axis=1),1)
+        covar_cl_w = np.dot((X_cl - mu_cl), (X_cl - mu_cl).T)/X_cl.shape[1]
+        covar_cl_b = np.dot((X_cl - mu), (X_cl - mu).T)/X_cl.shape[1]
+        S_w = S_w + covar_cl_w
+        S_b = S_b + covar_cl_b
+
+    l, U = eig(np.dot(np.linalg.pinv(S_w),S_b))
+    # exit()
+    # 2D Multiclass LDA Projections
     dim = 2
-    X_trans = np.dot(U.T[0:dim], X)
+    X_trans = np.real(np.dot(U.T[0:dim], X))
 
     plt.scatter(X_trans[0, y == 1], X_trans[1, y == 1], c='red', label='Class 1', alpha=0.75)
     plt.scatter(X_trans[0, y == 2], X_trans[1, y == 2], c='blue', label='Class 2', alpha=0.75)
     plt.scatter(X_trans[0, y == 3], X_trans[1, y == 3], c='green', label='Class 3', alpha=0.75)
-    plt.title("PCA for dimension = 2 for 'data-dimred-X.csv'")
-    plt.savefig("PCA_dim2.pdf", facecolor='w', edgecolor='w', papertype=None, format='pdf',
+    plt.title("LDA for dimension = 2 for 'data-dimred-X.csv'")
+    plt.savefig("LDA_dim2.pdf", facecolor='w', edgecolor='w', papertype=None, format='pdf',
                 transparent=False, bbox_inches='tight', pad_inches=0.1)
     plt.legend()
     plt.show()
 
-    # 3D PCA Projections
+    # 3D Multiclass LDA Projections
     dim = 3
     X_trans = np.real(np.dot(U.T[0:dim], X))
     # print(X_trans)
+    # for i in range(X_trans.shape[1]):
+    #     print(X_trans[2,i])
     fig = plt.figure()
-    ax = plt.axes(projection = '3d')
+    ax = plt.axes(projection='3d')
     ax.scatter(X_trans[0, y == 1], X_trans[1, y == 1], X_trans[2, y == 1], c='red', label='Class 1', alpha=0.75)
     ax.scatter(X_trans[0, y == 2], X_trans[1, y == 2], X_trans[2, y == 2], c='blue', label='Class 2', alpha=0.75)
     ax.scatter(X_trans[0, y == 3], X_trans[1, y == 3], X_trans[2, y == 3], c='green', label='Class 3', alpha=0.75)
-    ax.set_title("PCA for dimension = 3 for 'data-dimred-X.csv'")
-    plt.savefig("PCA_dim3.pdf", facecolor='w', edgecolor='w', papertype=None, format='pdf',
+    ax.set_title("LDA for dimension = 3 for 'data-dimred-X.csv'")
+    plt.savefig("LDA_dim3.pdf", facecolor='w', edgecolor='w', papertype=None, format='pdf',
                 transparent=False, bbox_inches='tight', pad_inches=0.1)
     ax.legend()
     plt.show()
